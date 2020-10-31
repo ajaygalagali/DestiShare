d4356a4a872da6127780b989c9af3e27ba76d410
ajaygalagali <gvajay321@gmail.com>
2020-10-31 20:33:18 +0530

    SplashScreen Added

--- app/src/main/java/com/astro/destishare/ui/HomeActivity.kt
+++ app/src/main/java/com/astro/destishare/ui/HomeActivity.kt
@@ -23,6 +23,7 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_home)
 
         auth = FirebaseAuth.getInstance()
